2dd3bdccb925dac3d146b0164d3165d03a08747f
Jose Varela <varelaj298@gmail.com>
2023-03-23 18:16:14 -0300

    add tp2_1_1.c

---
+++ tp2_1_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+#define N 20
+    int i;
+    double vt[N];
+    for (i = 0; i < N; i++) {
+        vt[i] = 1 + rand() % 100;
+        printf("%f ", vt[i]);
+    }
+    return 0;
+}
